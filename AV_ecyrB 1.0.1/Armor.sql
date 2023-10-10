fd59613c3aaae39b4803554c4ef4a430cf567b59
Ardo <90957355+axdevelopment@users.noreply.github.com>
2023-10-10 12:19:58 +0200

    Update Armor.sql

--- AV_ecyrB 1.0.1/Armor.sql
+++ AV_ecyrB 1.0.1/Armor.sql
@@ -1,4 +1,3 @@
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('bulletproof', 'Bullet-Proof Vest', 1, 0, 1),
-	('bulletproof50', 'Bullet-Proof 50%', 1, 0, 1)
-;
+	('bulletproof50', 'Bullet-Proof 50%', 1, 0, 1);
