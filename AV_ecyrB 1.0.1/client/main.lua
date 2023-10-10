7968460f14e37ca2f920cbcd89701fa740b74678
Ardo <90957355+axdevelopment@users.noreply.github.com>
2023-10-10 12:10:33 +0200

    Update main.lua
    added fancy notify & item check

--- AV_ecyrB 1.0.1/client/main.lua
+++ AV_ecyrB 1.0.1/client/main.lua
@@ -1,6 +1,26 @@
+ESX = exports["es_extended"]:getSharedObject()
+
+function mugshotNotify(titel, undertitle, message)
+	local mugshot, mugshotStr = ESX.Game.GetPedMugshot(GetPlayerPed(-1))
+	ESX.ShowAdvancedNotification(titel, undertitle, message, mugshotStr, 1)
+	UnregisterPedheadshot(mugshot)
+end
+
 RegisterNetEvent('armor:addArmor')
 AddEventHandler('armor:addArmor', function(amount)
-	AddArmourToPed(GetPlayerPed(-1), amount)
+	ESX.TriggerServerCallback('hasItem', function(hasItem, count)
+		if hasItem then
+			AddArmourToPed(GetPlayerPed(-1), amount)
+			if amount == 100 then
+				TriggerServerEvent('removeItem', 'bulletproof', 1)
+				mugshotNotify(Config.Translation.Title, Config.Translation.Undertitle, Config.Translation.Text100) 
+			else
+				TriggerServerEvent('removeItem', 'bulletproof50', 1)
+				mugshotNotify(Config.Translation.Title, Config.Translation.Undertitle, Config.Translation.Text50) 
+			end
+		else
+			mugshotNotify(Config.Translation.Title, Config.TranslationUndertitleNoItem, Config.Translation.TextNoItem)
+		end
+	end, item, count)
 end)
 
--- Credit to helping me improve this script goes to DrAceMisanthrope. Thanks for the help!
