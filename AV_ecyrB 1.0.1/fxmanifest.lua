698c10d0bdde272316c5602625274ee7e2159aec
Ardo <90957355+axdevelopment@users.noreply.github.com>
2023-10-10 12:19:28 +0200

    Update fxmanifest.lua

--- AV_ecyrB 1.0.1/fxmanifest.lua
+++ AV_ecyrB 1.0.1/fxmanifest.lua
@@ -3,20 +3,15 @@ games { 'gta5' }
 
 author 'Discord = ecyrB#6661 Email = Buckygaming050@gmail.com'
 description 'Simple armor script'
-version '1.0.0'
+version '1.1.0'
 
 client_scripts {
-	'@es_extended/locale.lua',
-	'locales/*.lua',
 	'config.lua',
 	'client/main.lua'
 }
 
 server_scripts {
 	"@mysql-async/lib/MySQL.lua",
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
 	'server/main.lua'
 }
 
