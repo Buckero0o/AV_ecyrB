7abde629a19d5a9e46213e5aa1f51eb35e118b20
Ardo <90957355+axdevelopment@users.noreply.github.com>
2023-10-10 12:17:47 +0200

    Update config.lua
    Crunched translations into it

--- AV_ecyrB 1.0.1/config.lua
+++ AV_ecyrB 1.0.1/config.lua
@@ -1,3 +1,9 @@
 Config = {}
-Config.Locale = 'en'
-Config.noti = true
+Config.Translation = {
+  Title = 'System',
+  Undertitle = 'Bulletproof',
+  UnderTitleNoItem = 'Bulletproof',
+  Text100 = 'You applied a 100 armor vest',
+  Text50 = 'You applied a 50 armor vest',
+  TextNoItem = 'You do not have a bulletproof vest to apply'
+} 
