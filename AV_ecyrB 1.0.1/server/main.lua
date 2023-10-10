d41c55ebf4c20b87609869a9910a8b12065e8533
Ardo <90957355+axdevelopment@users.noreply.github.com>
2023-10-10 12:21:43 +0200

    Update main.lua
    Comments 'n shit

--- AV_ecyrB 1.0.1/server/main.lua
+++ AV_ecyrB 1.0.1/server/main.lua
@@ -20,6 +20,12 @@ AddEventHandler('removeItem', function(item, count)
 	xPlayer.removeInventoryItem(item, count)
 end)
 
+--[[
+very handy cb, can be utilized in other scripts aswell
+item = item from db as @STRING
+count = itemcount you need to have to return cb(true) as @INT
+]]
+
 ESX.RegisterServerCallback('hasItem', function(source, cb, item, count)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	if xPlayer then
